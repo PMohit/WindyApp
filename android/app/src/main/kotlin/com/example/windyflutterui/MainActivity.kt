26423a688a116bf41b83669a12c90e536d607dc6
Mohit <mohit.padhye2011@gmail.com>
2020-08-30 16:22:20 +0545

    added login screen

---
+++ android/app/src/main/kotlin/com/example/windyflutterui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.windyflutterui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/helper/contants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Contants {
+  static Color purplish = Color(0xff4e3d77);
+  static Color orangish = Color(0xffffac00);
+  static Color greyish = Color(0xff656566);
+}
+
+ 
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF6CA8F1),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
